8bc265873f1f997c23a6893f49fab7d4439075be
0byteme <819643718@qq.com>
2025-12-16 11:40:13 +0800

    update

--- rpc/namespaces/ethereum/debank/api_multicall.go
+++ rpc/namespaces/ethereum/debank/api_multicall.go
@@ -8,37 +8,12 @@ import (
 	"sync"
 	"time"
 
-	"cosmossdk.io/log"
-	"github.com/cosmos/cosmos-sdk/client"
-	"github.com/cosmos/cosmos-sdk/server"
 	"github.com/cosmos/evm/rpc/backend"
 	rpctypes "github.com/cosmos/evm/rpc/types"
 	evmtypes "github.com/cosmos/evm/x/vm/types"
 	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 )
-
-type API struct {
-	ctx         *server.Context
-	logger      log.Logger
-	backend     backend.EVMBackend
-	clientCtx   client.Context
-	queryClient *rpctypes.QueryClient
-}
-
-func NewAPI(
-	ctx *server.Context,
-	backend backend.EVMBackend,
-	clientCtx client.Context,
-) *API {
-	return &API{
-		ctx:         ctx,
-		logger:      ctx.Logger.With("module", "pre"),
-		backend:     backend,
-		clientCtx:   clientCtx,
-		queryClient: rpctypes.NewQueryClient(clientCtx),
-	}
-}
 
 const (
 	singleCallTimeout = 5 * time.Second
