efce71de5f5be06605b1f6edfaad18f7fabd119c
0byteme <819643718@qq.com>
2026-01-08 15:43:36 +0800

    feat: remove etcd register (#7)

--- server/flags/flags.go
+++ server/flags/flags.go
@@ -73,11 +73,6 @@ const (
 	TLSKeyPath  = "tls.key-path"
 )
 
-// ETCDConfig flags
-const (
-	ETCDConfig = "etcd-config"
-)
-
 // AddTxFlags adds common flags for commands to post tx
 func AddTxFlags(cmd *cobra.Command) (*cobra.Command, error) {
 	cmd.PersistentFlags().String(flags.FlagChainID, "", "Specify Chain ID for sending Tx")
